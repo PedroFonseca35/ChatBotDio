23e66caca39957a511ad31f0bb82647a2606e886
PedroFonseca35 <pedrorecife35@gmail.com>
2026-04-25 11:55:56 -0300

    desempenho

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import requests
 import streamlit as st
+import random
 
 # Configuração ollama serve
 OLLAMA_URL = "http://localhost:11434/api/generate"
@@ -17,18 +18,19 @@ historico = pd.read_csv('./data/historico_atendimento.csv')
 usuario = perfil['usuario']
 
 contexto = f"""
-CLIENTE: {usuario.get('idade', 'N/A')} anos, perfil {usuario.get('perfil_investidor', 'N/A')}
-OBJETIVO: {usuario.get('objetivo_principal', 'N/A')}
-PATRIMÔNIO: R$ {usuario.get('patrimonio_total', 'N/A')} | RESERVA: R$ {usuario.get('reserva_emergencia_atual', 'N/A')}
+
+Perfil: {usuario.get('perfil_investidor', 'N/A')}
+Objetivo: {usuario.get('objetivo_principal', 'N/A')}
+Reserva: {usuario.get('reserva_emergencia_atual','N/A')}
 
 TRANSAÇÕES RECENTES:
-{transacoes.to_string(index=False)}
+{transacoes.sample(3).to_string(index=False)}
 
 ATENDIMENTOS ANTERIORES:
-{historico.to_string(index=False)}
+{historico.sample(3).to_string(index=False)}
 
 PRODUTOS DISPONÍVEIS:
-{json.dumps(produtos, indent=2, ensure_ascii=False)}
+{json.dumps(random.sample(produtos, 3), ensure_ascii=False)}
 """
 # SYSTEM PROMPT
 SYSTEM_PROMPT = """
@@ -36,21 +38,15 @@ Você é o LUMI, um educador financeiro amigável e didático. Seu meta é ajuda
 Objetivo:
 Ensinar conceitos de finanças pessoais de forma simples, usando os dados do cliente como exemplos práticos.
 REGRAS:
-1. Sempre baseie suas repostas nos dados fornecidos;
-2. Nunca recomende investimentos específicos - apenas como funcionam;
-3. Use os dados fornecidos para dar exemplos personalizados;
-4. Linguagem simples, como se explicasse para um amigo;
-5. Se não souber algo, admita: "Não tenho essa informação, mas posso explicar...";
-6. Sempre pergunte se o cliente entendeu;
-7. Nunca invente informações financeiras;
-8. Não faça previsões de lucro ou garantia de retorno financeiro;
-9. Não incentive decisões impulsivas ou de risco;
-10. Sempre explique riscos quando falar de qualquer tipo de investimento;
-11. Evite termos técnicos complexos; quando usar, explique de forma simples;
-12. Mantenha respostas curtas, organizadas e fáceis de entender;
-13. Ao mostrar termos técnicos explicar diretamente no que ele é usado e não colocar palavras que enfeite as respostas do tipo "extremamente" "bacana" e afins;
-14. Não substitua orientação profissional (contador, assessor, etc.);
-15. Não solicite ou utilize dados sensíveis do usuário (senha, dados bancários);
+Use apenas dados fornecidos
+Não recomende investimentos específicos
+Explique riscos
+Linguagem simples
+Não invente informações, admita: Não tenho essa informação
+Não garantia de retorno financeiro
+Não substitua orientação profissional (contador, assessor, etc.)
+Não solicite ou utilize dados sensíveis
+Não peça dados sensíveis
 """
 
 # Chamar ollama
